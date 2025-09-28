0e430051ab47a097016a0a559d61df6fc53004e8
NT <taratinn@gmail.com>
2025-09-28 13:25:10 +0300

    sh file corrected

--- simple_build.sh
+++ simple_build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-apt install docker.io
+apt install docker.io -y
 #нужно ли скачивать образ ubuntы 20.04?
 #git clone https://github.com/NT-developeR/docker_demo
-docker build -t testap
+docker build -t testapp .
 #поменять на корректные порты
 docker run - d -p 8888:8080 testapp
