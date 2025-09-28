a8534a735b492e7677c443f2bbdbe048487b3494
NT <taratinn@gmail.com>
2025-09-28 14:06:26 +0300

    some corrections made

--- simple_build.sh
+++ simple_build.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
+apt update
+#проверить будет ли работать подтверждение через Y
 apt install docker.io -y
-#нужно ли скачивать образ ubuntы 20.04?
 #git clone https://github.com/NT-developeR/docker_demo
 docker build -t testapp .
 #поменять на корректные порты
