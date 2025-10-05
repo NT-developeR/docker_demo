4be770661c901dccf99eacc6c464a18e7593fc0a
NT <taratinn@gmail.com>
2025-10-05 17:56:28 +0300

    corrections

--- docker_task/simple_build.sh
+++ docker_task/simple_build.sh
@@ -2,7 +2,6 @@
 apt update
 #проверить будет ли работать подтверждение через Y
 apt install docker.io -y
-#git clone https://github.com/NT-developeR/docker_demo
 docker build -t testapp .
 #поменять на корректные порты
-docker run - d -p 8888:8080 testapp
+docker run -d -p 8888:8080 testapp
